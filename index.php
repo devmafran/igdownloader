43e974b97b184854e8396e85151e95c520fb399a
Adhika Mafran <dev.mafran@outlook.com>
2023-05-12 16:14:00 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,9 @@
+
+
+<a href="https://api.instagram.com/oauth/authorize?client_id=549784487358718&redirect_uri=http://mafran2.local/&scope=user_profile,user_media&response_type=code">Authorize App</a><br/>
+<br/>
+<br/>
+https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret={instagram-app-secret}&access_token={short-lived-access-token}
+<br/>
+<a href="https://graph.instagram.com/3086301200686624549?fields=id,media_type,media_url,username,timestamp&access_token=IGQVJWb0YxWUpVT1VIemhMekZA6YUtxTFJ0eDF3eUdrQVZAhRkZAjN2pFTWtlaHZAnV1ExNzczUEhCeFByZAmxyWmgxemw0dFRXZA2JhaDdGajRSbnl0M3hBR3FaZAFVzMFhnZAkhzTXFzMVpPTVBaLS1JRVJuMQZDZD">Test</a>
+<br/><br/>IGQVJWb0YxWUpVT1VIemhMekZA6YUtxTFJ0eDF3eUdrQVZAhRkZAjN2pFTWtlaHZAnV1ExNzczUEhCeFByZAmxyWmgxemw0dFRXZA2JhaDdGajRSbnl0M3hBR3FaZAFVzMFhnZAkhzTXFzMVpPTVBaLS1JRVJuMQZDZD
